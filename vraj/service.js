48740782563cc9e3fb1736b56e1bab395ff679c6
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2024-12-06 11:27:24 +0530

    Add files via upload

---
+++ vraj/service.js
@@ -0,0 +1,6 @@
+// document.querySelectorAll('.service-card').forEach(card => {
+    //   card.addEventListener('click', () => {
+    //     const feature = card.querySelector('h3')?.innerText || 'Unknown feature';
+    //     alert(Feature selected: ${feature});
+    //   });
+    // });
